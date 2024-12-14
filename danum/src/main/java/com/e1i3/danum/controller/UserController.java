a097674791a3d4dd3a99a2feb888936214550252
추호성 <hosung@chuhoseong-ui-MacBookAir.local>
2024-12-14 21:42:07 +0900

    Feat: update status

--- danum/src/main/java/com/e1i3/danum/controller/UserController.java
+++ danum/src/main/java/com/e1i3/danum/controller/UserController.java
@@ -2,7 +2,9 @@ package com.e1i3.danum.controller;
 
 import com.e1i3.danum.dto.request.CreateUserRequest;
 import com.e1i3.danum.dto.response.CreateUserResponse;
+import com.e1i3.danum.request.UpdateStatusToStoreRequest;
 import com.e1i3.danum.response.ReadStoreResponses;
+import com.e1i3.danum.response.UpdateStatusToStoreResponse;
 import com.e1i3.danum.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -19,10 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     private final UserService userService;
     @PostMapping("/user/sign")
-    public ResponseEntity<CreateUserResponse> readStore(@RequestBody CreateUserRequest createUserRequest){
+    public ResponseEntity<CreateUserResponse> createUser(@RequestBody CreateUserRequest createUserRequest){
         CreateUserResponse createUserResponse = userService.createUser(createUserRequest);
 
         return ResponseEntity.status(HttpStatus.OK).body(createUserResponse);
     }
+    @PostMapping("/user/status-store")
+    public ResponseEntity<UpdateStatusToStoreResponse> updateStatusToStore(@RequestBody UpdateStatusToStoreRequest updateStatusToStoreRequest) throws Exception {
+        UpdateStatusToStoreResponse updateStatusToStoreResponse = userService.updateStatusToStore(updateStatusToStoreRequest);
 
+        return ResponseEntity.status(HttpStatus.OK).body(updateStatusToStoreResponse);
+    }
 }
