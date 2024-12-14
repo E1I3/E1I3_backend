cc5ad02e083dd271ee2de08536913c024fad597a
jiwonLee42 <ljw56b@gmail.com>
2024-12-15 03:32:12 +0900

    Feat: mypage api 구현

--- danum/src/main/java/com/e1i3/danum/dto/response/MyPageSellerResponseDto.java
+++ danum/src/main/java/com/e1i3/danum/dto/response/MyPageSellerResponseDto.java
@@ -1,10 +1,33 @@
 package com.e1i3.danum.dto.response;
 
+import com.e1i3.danum.entity.Store;
+import com.e1i3.danum.entity.User;
+import com.e1i3.danum.enumeration.StoreType;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class MyPageSellerResponseDto {
+
+    private Long userId;
+    private String userName;
+    private Long userBalance;
+    private String userEmail;
+    private String storeName;
+    private StoreType storeType;
+    private String storeAddress;
+
+    @Builder
+    public MyPageSellerResponseDto(Store store, User user) {
+        this.userId = user.getUserId();
+        this.userName = user.getName();
+        this.userBalance = user.getBalance();
+        this.userEmail = user.getEmail();
+        this.storeName = store.getStoreName();
+        this.storeType = store.getStoreType();
+        this.storeAddress = store.getAddress();
+    }
 
 }
--- danum/src/main/java/com/e1i3/danum/service/UserService.java
+++ danum/src/main/java/com/e1i3/danum/service/UserService.java
@@ -2,12 +2,15 @@ package com.e1i3.danum.service;
 
 import com.e1i3.danum.dto.request.CreateUserRequest;
 import com.e1i3.danum.dto.response.CreateUserResponse;
+import com.e1i3.danum.dto.response.MyPageSellerResponseDto;
+import com.e1i3.danum.dto.response.MyPageUserResponseDto;
 import com.e1i3.danum.entity.Store;
 import com.e1i3.danum.entity.User;
 import com.e1i3.danum.repository.StoreRepository;
 import com.e1i3.danum.repository.UserRepository;
 import com.e1i3.danum.request.UpdateStatusToStoreRequest;
 import com.e1i3.danum.response.UpdateStatusToStoreResponse;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -50,6 +53,25 @@ public class UserService {
                 .storeId(updateStatusToStoreRequest.getStoreId())
                 .build();
     }
+
+    // 판매자 마이페이지 조회
+    @Transactional
+    public MyPageSellerResponseDto viewSellerPage(Long storeId, Long userId){
+        Store store = storeRepository.findById(storeId)
+                .orElseThrow(() -> new IllegalArgumentException("가게를 찾을 수 없습니다. : " + storeId));
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다. : " + userId));
+        return new MyPageSellerResponseDto(store, user);
+    }
+
+    // 사용자 마이페이지 조회
+    @Transactional
+    public MyPageUserResponseDto viewUserPage(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("유저를 찾을 수 없습니다. : " + userId));
+        return new MyPageUserResponseDto(user);
+    }
+
 
 
 }
