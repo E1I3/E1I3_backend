0bfdff4af6d79c73b5fad4a0cd67ca90e5630b4e
추호성 <hosung@chuhoseong-ui-MacBookAir.local>
2024-12-14 14:39:46 +0900

    Feat: init spring project

---
+++ danum/src/main/java/com/e1i3/danum/DanumApplication.java
@@ -0,0 +1,13 @@
+package com.e1i3.danum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanumApplication.class, args);
+	}
+
+}
---
+++ danum/src/test/java/com/e1i3/danum/DanumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e1i3.danum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
