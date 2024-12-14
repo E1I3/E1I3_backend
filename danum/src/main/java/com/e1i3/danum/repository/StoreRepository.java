1d5181d437e21220d34233ef18105b00b64bdf2a
추호성 <hosung@chuhoseong-ui-MacBookAir.local>
2024-12-14 22:57:42 +0900

    Fix: update storeRepository

--- danum/src/main/java/com/e1i3/danum/repository/StoreRepository.java
+++ danum/src/main/java/com/e1i3/danum/repository/StoreRepository.java
@@ -2,10 +2,14 @@ package com.e1i3.danum.repository;
 
 import com.e1i3.danum.entity.Store;
 import com.e1i3.danum.entity.User;
+import com.e1i3.danum.repository.custom.StoreRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
-public interface StoreRepository extends JpaRepository<Store, Long> {
+public interface StoreRepository extends JpaRepository<Store, Long>, StoreRepositoryCustom {
     Optional<Store> findByUser(User currentUser);
+    @Query("SELECT s FROM stores s WHERE s.user.userId = :userId")
+    Store findByUserId(Long userId);
 }
